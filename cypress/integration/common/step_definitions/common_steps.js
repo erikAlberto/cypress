82e8d30261457daefdbc76d0adf3518feaf28bfd
Alberto_Vargas <alberto.vargas@jalasoft.com>
2021-07-05 14:45:32 -0400

    updating space between step definitions

--- cypress/integration/common/step_definitions/common_steps.js
+++ cypress/integration/common/step_definitions/common_steps.js
@@ -3,9 +3,11 @@
 Given('I open the web app url', () => {            
   cy.visit('https://opensource-demo.orangehrmlive.com/index.php/auth/login');
 }); 
+
 Then( 'I verify title of web page as {string}', title => {    
   cy.title().should('include', title); 
 });
+
 And( 'I go to {string}', value => {
   cy.get(`#menu_${value}_viewDirectory`).click(); 
   
